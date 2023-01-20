f8b9f86dae6661fbe6bd6988fe171bebf40a9f0c
nihal <nihalpuliyakkady@gmail.com>
2023-01-20 16:54:50 +0530

    [ADD] website files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
+const App = () => (
+  <div>
+    <div>
+    
+      <Navbar />
+       <Header /> 
+    </div>
+    {/* <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer /> */}
+  </div>
+);
+
+
+
+export default App;
---
+++ src/components/feature/Features.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Feature = () => {
+  return (
+    <div>
+        qwerttyy
+    </div>
+  )
+}
+
+export default Feature
